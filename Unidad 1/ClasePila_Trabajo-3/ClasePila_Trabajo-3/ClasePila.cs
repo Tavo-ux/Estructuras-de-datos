2952ef8b83f640450c1da24a1a968196371fb72d
Tavo-ux <alejandrocoto928@gmail.com>
2025-10-14 21:07:42 -0500

    clase de la pila

---
+++ Unidad 1/ClasePila_Trabajo-3/ClasePila_Trabajo-3/ClasePila.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasePila_Trabajo_3
+{
+    internal class ClasePila<Tipo> where Tipo : IEquatable<Tipo>
+    {
+		private ClaseNodo<Tipo> _TopeLista;
+
+		public ClaseNodo<Tipo> Topelista
+		{
+			get { return _TopeLista; }
+			set { _TopeLista = value; }
+		}
+
+        public ClasePila()
+        {
+            _TopeLista = null;
+        }
+
+        public bool Vacia 
+        {
+            get
+            {
+                if ( Topelista == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+
+                }
+            }
+        }
+
+        public void Push(Tipo Objeto)
+        {
+            if (Vacia)
+            {
+                ClaseNodo<Tipo> nodoNuevo = new ClaseNodo<Tipo>();
+                nodoNuevo.ObjetoConDatos = Objeto;
+                nodoNuevo.Siguiente = null;
+                Topelista = nodoNuevo;
+                return;
+            }
+            else
+            {
+                ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+                nodoActual = Topelista;
+                do
+                {
+                    if (Objeto.Equals(nodoActual.ObjetoConDatos))
+                    {
+                        throw new Exception("El objeto ya existe en la pila");
+                    }
+                    else
+                    {
+                        nodoActual = nodoActual.Siguiente;
+                    }
+                }
+                while (nodoActual != null);
+                ClaseNodo<Tipo> nodoNuevo = new ClaseNodo<Tipo>();
+                nodoNuevo.ObjetoConDatos = Objeto;
+                nodoNuevo.Siguiente = Topelista;
+                Topelista = nodoNuevo;
+                return;
+            }
+
+        }
+
+        public Tipo Pop()
+        {
+            ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+            ClaseNodo<Tipo> nodoEliminado = new ClaseNodo<Tipo>();
+            nodoActual = Topelista;
+            if (Vacia)
+            {
+                throw new Exception("La pila esta vacia");
+            }
+            else
+            {
+                Topelista = nodoActual.Siguiente;
+                nodoEliminado = nodoActual;
+                nodoActual = null;
+                return nodoEliminado.ObjetoConDatos;
+            }
+        }
+
+        public Tipo Pop(Tipo Objeto)
+        {
+            if (Vacia)
+            {
+                throw new Exception("La pila esta vacia");
+            }
+            else
+            {
+                ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+                ClaseNodo<Tipo> nodoPrevio = new ClaseNodo<Tipo>();
+                nodoActual = Topelista;
+                nodoPrevio = Topelista;
+                do
+                {
+                    if (Objeto.Equals(nodoActual.ObjetoConDatos))
+                    {
+                        ClaseNodo<Tipo> nodoEliminado = new ClaseNodo<Tipo>();
+                        nodoEliminado = nodoActual;
+                        if (nodoActual == Topelista)
+                        {
+                            Topelista = nodoActual.Siguiente;
+                            nodoActual = null;
+                            return nodoEliminado.ObjetoConDatos;
+                        }
+                        else
+                        {
+                            nodoPrevio.Siguiente = nodoActual.Siguiente;
+                            nodoActual = null;
+                            return nodoEliminado.ObjetoConDatos;
+                        }
+
+                    }
+                    else
+                    {
+                        nodoPrevio = nodoActual;
+                        nodoActual = nodoActual.Siguiente;
+                    }
+                }
+                while (nodoActual != null);
+                throw new Exception("El objeto no existe en la pila");
+            }
+        }
+
+        public Tipo BuscarNodo(Tipo Objeto)
+        {
+            if (Vacia)
+            {
+                throw new Exception("La pila esta vacia");
+            }
+            else
+            {
+                ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+                nodoActual = Topelista;
+                do
+                {
+                    if (Objeto.Equals(nodoActual.ObjetoConDatos))
+                    {
+                        return nodoActual.ObjetoConDatos;
+                    }
+                    else
+                    {
+                        nodoActual = nodoActual.Siguiente;
+                    }
+                }
+                while (nodoActual != null);
+                throw new Exception("El objeto no existe en la pila");
+            }
+        }
+
+        public void Vaciar()
+        {
+            if (Vacia)
+            {
+                return;
+            }
+            else
+            {
+                ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+                ClaseNodo<Tipo> nodoPrevio = new ClaseNodo<Tipo>();
+                nodoActual = Topelista;
+                nodoPrevio = Topelista;
+                do
+                {
+                    nodoPrevio = nodoActual;
+                    nodoActual = nodoActual.Siguiente;
+                    nodoPrevio = null;
+                }
+                while (nodoActual != null);
+                Topelista = null;
+                return;
+            }
+        }
+
+        public IEnumerator<Tipo> GetEnumerator()
+        {
+            if (Vacia)
+            {
+                yield break;
+            }
+            else
+            {
+                ClaseNodo<Tipo> nodoActual = new ClaseNodo<Tipo>();
+                nodoActual = Topelista;
+                do
+                {
+                    yield return nodoActual.ObjetoConDatos;
+                    nodoActual = nodoActual.Siguiente;
+                }
+                while (nodoActual != null);
+                yield break;
+            }
+        }
+    }
+}
---
+++ Unidad 1/ClasePila_Trabajo-3/ClasePila_Trabajo-3/Edificio.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasePila_Trabajo_3
+{
+    public class Edificio : IEquatable<Edificio>
+    {
+        private string _strNombre;
+
+        public string Nombre
+        {
+            get { return _strNombre; }
+            set { _strNombre = value; }
+        }
+
+        private DateTime _dtmFechaConstruccion;
+
+        public DateTime FechaConstruccion
+        {
+            get { return _dtmFechaConstruccion; }
+            set { _dtmFechaConstruccion = value; }
+        }
+
+        private double _dblAltura;
+
+        public double Altura
+        {
+            get { return _dblAltura; }
+            set { _dblAltura = value; }
+        }
+
+        private int _intClave;
+
+        public int Clave
+        {
+            get { return _intClave; }
+            set { _intClave = value; }
+        }
+
+        private char _chrEstilo;
+
+        public char Estilo
+        {
+            get { return _chrEstilo; }
+            set { _chrEstilo = value; }
+        }
+
+        private bool _blnRenovado;
+
+        public bool Renovado
+        {
+            get { return _blnRenovado; }
+            set { _blnRenovado = value; }
+        }
+
+        private string _strEpoca;
+
+        public string Epoca
+        {
+            get { return _strEpoca; }
+            set { _strEpoca = value; }
+        }
+
+
+        private string _strImagen;
+
+        public string Imagen
+        {
+            get { return _strImagen; }
+            set { _strImagen = value; }
+        }
+
+        public bool Equals(Edificio otroEdificio)
+        {
+            if (this.Clave == otroEdificio.Clave)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
