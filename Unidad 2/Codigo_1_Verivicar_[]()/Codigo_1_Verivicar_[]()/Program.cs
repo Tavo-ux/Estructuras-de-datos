88493daa204a8922d44c2d8a787b4120addf02b0
Tavo-ux <alejandrocoto928@gmail.com>
2025-10-15 19:44:46 -0500

    1er codigo de los simbolos

---
+++ Unidad 2/Codigo_1_Verivicar_[]()/Codigo_1_Verivicar_[]()/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codigo_1_Verivicar_____
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una cadena con parentesis, corchetes o llaves: ");
+            string cadena = Console.ReadLine();
+
+            if (VerificarCadena(cadena))
+                Console.WriteLine(" Correcto");
+            else
+                Console.WriteLine(" Error");
+
+            Console.WriteLine("\nPresione una tecla para salir");
+            Console.ReadKey();
+        }
+
+        static bool VerificarCadena(string cadena)
+        {
+            Stack<char> pila = new Stack<char>();
+
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                char f = cadena[i];
+
+                // Si es un símbolo de apertura, se inserta en la pila
+                if (f == '(' || f == '[' || f == '{')
+                {
+                    pila.Push(f);
+                }
+                // Si es de cierre
+                else if (f == ')' || f == ']' || f == '}')
+                {
+                // Si la pila está vacía es igual a error
+                    if (pila.Count == 0)
+                        return false;
+
+                    char dentro = pila.Pop();
+
+                // Verificar que esten correctos
+                    if ((f == ')' && dentro != '(') || (f == ']' && dentro != '[') || (f == '}' && dentro != '{'))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // Si al final la pila está vacía, la cadena es correcta
+            return pila.Count == 0;
+        
+        }
+    }
+}
